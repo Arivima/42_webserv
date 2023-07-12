fa8689ca1bf5a0b16b556e40e38aa8f58f14f3a3
Arivima <arielle.villamassone@gmail.com>
2023-07-12 14:21:15 +0200

    clean

--- cclean.sh
+++
@@ -1,9 +0,0 @@
-rm -rf /nfs/homes/$USER/.var/app/com.google.Chrome/config/google-chrome/Default/Service Worker/CacheStorage;
-rm -rf /nfs/homes/$USER/.var/app/com.google.Chrome/cache;
-rm -rf /nfs/homes/$USER/.var/app/com.slack.Slack/config/Slack/Service Worker/CacheStorage;
-rm -rf /nfs/homes/$USER/.var/app/com.slack.Slack/config/Slack/Cache;
-rm -rf /nfs/homes/$USER/.var/app/com.slack.Slack/cache;
-rm -rf /nfs/homes/$USER/.var/app/com.visualstudio.code/config/Code/CachedData;
-rm -rf /nfs/homes/$USER/.var/app/com.jetbrains.PhpStorm/cache;
-rm -rf /nfs/homes/$USER/.mozilla/firefox/3n8ozw4e.default-release/storage/default;
-rm -rf /nfs/homes/$USER/.cache;
